d4036c5e40dade7a1a2e01cdd0625255d6de454e
Benedithos <johnsjbinas@gmail.com>
2025-02-23 20:42:20 +0800

    Installed and cleaned the basic react typescript environment

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./index.css";
+function App() {
+  return (
+    <>
+      <h1>Title</h1>
+      <p>Paragraph</p>
+    </>
+  );
+}
+
+export default App;
